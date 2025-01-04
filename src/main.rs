456bcb5b76979f522fb2e67666393a46c8ea3039
Emily Eldar <eldaremily@gmail.com>
2025-01-03 17:37:19 -0800

    Return multiple pongs if needed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
 use std::io::Write;
+use std::io::Read;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,7 +9,16 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buf = vec![0;1024];
+                let _ = _stream.read(&mut buf);
+                let string_buf = String::from_utf8(buf).unwrap();
+                println!("{}", string_buf);
+
+                let parts = string_buf.split("\n");
+
+                for _part in parts {
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
